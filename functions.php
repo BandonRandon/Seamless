c47d999a26d2a268391738e5a224b45d015ede84
James <james@seamlessthemes.com>
2012-12-15 21:00:59 -0500

    Some changes!

--- functions.php
+++ functions.php
@@ -170,9 +170,11 @@ function seamless_scripts() {
 
 	wp_register_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
 	wp_register_script( 'html5-shiv', get_template_directory_uri() . '/js/html5-shiv.js', array( 'jquery' ), '20120206', true );
+	wp_register_script( 'seamless', get_template_directory_uri() . '/js/seamless.js', array( 'jquery' ), '20120206', true );
 
 	wp_enqueue_script( 'small-menu' );
 	wp_enqueue_script( 'html5-shiv' );
+	wp_enqueue_script( 'seamless' );
 
 }
 
