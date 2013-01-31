ec4c2e7abcf8b6a1ad96aedc1920ba33bcb6ff14
James <james@seamlessthemes.com>
2013-01-30 22:16:42 -0500

    Round of updates

--- header.php
+++ header.php
@@ -39,10 +39,10 @@
 
 			<div class="wrap">
 
-				<div id="branding">
-					<?php hybrid_site_title(); ?>
-					<?php hybrid_site_description(); ?>
-				</div><!-- #branding -->
+				<hgroup id="branding">
+					<h1 id="site-title"><a href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+				</hgroup><!-- #branding -->
 
 				<?php get_sidebar( 'header' ); // Loads the sidebar-header.php template. ?>
 
